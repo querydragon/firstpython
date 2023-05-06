fec95da12d0108e8f307c7f3b8df0a0ab7ad8d43
querydragon <132686505+querydragon@users.noreply.github.com>
2023-05-06 22:24:13 +0900

    Add files via upload

---
+++ 9th.py
@@ -0,0 +1,16 @@
+# import 뒤에 나오는 이름은 라이브러리 또는 패키지
+# import 해당 라이브러리나 패키지를 불러들어오게 함
+import turtle
+
+# turtle.shape(모양)은 윈도우 창이 나오면서 동시에 거북이의 모양을 결정
+# arrow/turtle/circle/squeare/triangle/classic 중 하나로 설정
+
+turtle.shape('turtle')
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+
