95566ef80b9e025cecbfb465942127510f78ee14
querydragon <132686505+querydragon@users.noreply.github.com>
2023-05-06 21:40:22 +0900

    Add files via upload

---
+++ 4th.py
@@ -0,0 +1,10 @@
+num1 = int(input("숫자1 ====>"))
+num2 = int(input("숫자2 ====>"))
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+print(num1, "+", num2, "=", result1)
+print(num1, "-", num2, "=", result2)
+print(num1, "*", num2, "=", result3)
+print(num1, "/", num2, "=", result4)
